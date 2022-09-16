b1075e96d92005304c09838957523f067dfc30e9
Chrystian Ferreira <chrystian.profissional@gmail.com>
2022-09-15 21:05:16 -0300

    WIP - Vitest @ Program With Erik - https://www.youtube.com/watch?v=FJRuG85tXV0

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import View from "./components/View.vue"
+export default {
+  components: {
+    View
+  },
+}
+</script>
+
+<template>
+  <h2>Hello world</h2>
+  <View element="div">Hello from view</View>
+</template>
+
+<style scoped>
+#app { 
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
---
+++ src/components/View.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  element: string;
+  ariaLabel?: string;
+  isDisabled?: boolean;
+}>();
+</script>
+
+<template>
+  
+  <component 
+    :is="element" 
+    :aria-label="ariaLabel" 
+    :disabled="isDisabled"
+  >
+    <slot></slot>
+    <h3>Obv wrong</h3>
+  </component>
+</template>
